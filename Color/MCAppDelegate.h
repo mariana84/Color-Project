54fe147085ce0327106995e72a6b3b6d3f2d34a9
Mariana Carrillo <marianakerr@Mariana-Carrillos-MacBook-2.local>
2012-07-28 13:17:42 -0700

    Initial Commit

---
+++ Color/MCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MCAppDelegate.h
+//  Color
+//
+//  Created by Mariana Carrillo on 7/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MCViewController;
+
+@interface MCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MCViewController *viewController;
+
+@end
---
+++ Color/MCViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCViewController.h
+//  Color
+//
+//  Created by Mariana Carrillo on 7/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCViewController : UIViewController
+
+@end
